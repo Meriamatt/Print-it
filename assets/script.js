9783cb1622beaf16b34f7d770e46c5fe1f39880f
Meriam Attaiki <mer.attaiki@gmail.com>
2024-03-13 15:49:32 +0100

    handle click on arrows

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,21 @@
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
 ];
 
 let changeSlide = document.getElementsByClassName("banner-img")[0];
@@ -25,36 +26,30 @@ let arrowLeft = document.querySelector(".arrow_left");
 console.log(arrowLeft);
 let arrowRight = document.querySelector(".arrow_right");
 console.log(arrowRight);
-let selectedBullet =document.getElementsByClassName("")
-arrowRight.addEventListener("click", function(){ 
-	//console.log (index);
-	//console.log (nbrElement);
-	if (index < nbrElement-1){
-		index++;
-	}
-	else {
-		index = 0
-	}
-	changeSlide.src = "./assets/images/slideshow/" +slides[index].image;
-	changeText.innerHTML = slides[index].tagLine;
-	console.log("click right"); 
-	//console.log(index);
+let bullets = document.getElementsByClassName("dot");
+
+arrowRight.addEventListener("click", function () {
+  if (index < nbrElement - 1) {
+    index++;
+  } else {
+    index = 0;
+  }
+  changeSlide.src = "./assets/images/slideshow/" + slides[index].image;
+  changeText.innerHTML = slides[index].tagLine;
+  bullets[index].classList.add("dot_selected");
+  bullets[index - 1].classList.remove("dot_selected");
+  console.log("click right");
+
+  console.log("selected bullet" + index);
 });
-arrowLeft.addEventListener("click", function(){
-	console.log(index);
-	if (index < nbrElement && index > 0){
-			index--;
-		}
-		else {
-			index= nbrElement-1;
-		}
-		changeSlide.src = "./assets/images/slideshow/" +slides[index].image;
-		changeText.innerHTML = slides[index].tagLine;
-	 console.log("click left"); 
-	});
-
-
-
-
-
-
+arrowLeft.addEventListener("click", function () {
+  bullets[index].classList.remove("dot_selected");
+  if (index < nbrElement && index > 0) {
+    index--;
+  } else {
+    index = nbrElement - 1;
+  }
+  changeSlide.src = "./assets/images/slideshow/" + slides[index].image;
+  changeText.innerHTML = slides[index].tagLine;
+  bullets[index].classList.add("dot_selected");
+});
